20ac387088f3e659faac7e37af1e375e41d2148f
Basharath <basharath.in@gmail.com>
2021-02-24 22:05:48 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  // purge: ['./public/**/*.html', './public/**/*.js'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
